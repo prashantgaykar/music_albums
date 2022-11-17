609f75698aa7526078f308088ae11922b3dbf98f
prashant.gaykar@redmangoanalytics.com <pg7233c%>
2022-11-17 19:18:38 +0530

    removed activity title

--- app/src/main/java/io/prashant/topalbums/util/AppException.kt
+++
@@ -1,5 +0,0 @@
-package io.prashant.topalbums.util
-
-sealed class AppException(open val msg: String) : Exception(msg) {
-    class UnknownException(override val msg: String = "") : AppException(msg)
-}
